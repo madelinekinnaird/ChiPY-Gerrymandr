b1844b53c5c8c03a59c8ebf5ad69cdf999bccd45
Madeline Kinnaird <madeline.kinnaird@gmail.com>
2018-09-26 13:26:38 -0500

    succesful code to clean

--- gerrymander score/gerrymander_score_cleaner.py
+++ gerrymander score/gerrymander_score_cleaner.py
@@ -1,30 +1,83 @@
 import csv
 
+clean_data = []
 
+states = {
+        'AK': 'Alaska',
+        'AL': 'Alabama',
+        'AR': 'Arkansas',
+        'AS': 'American Samoa',
+        'AZ': 'Arizona',
+        'CA': 'California',
+        'CO': 'Colorado',
+        'CT': 'Connecticut',
+        'DC': 'District of Columbia',
+        'DE': 'Delaware',
+        'FL': 'Florida',
+        'GA': 'Georgia',
+        'GU': 'Guam',
+        'HI': 'Hawaii',
+        'IA': 'Iowa',
+        'ID': 'Idaho',
+        'IL': 'Illinois',
+        'IN': 'Indiana',
+        'KS': 'Kansas',
+        'KY': 'Kentucky',
+        'LA': 'Louisiana',
+        'MA': 'Massachusetts',
+        'MD': 'Maryland',
+        'ME': 'Maine',
+        'MI': 'Michigan',
+        'MN': 'Minnesota',
+        'MO': 'Missouri',
+        'MP': 'Northern Mariana Islands',
+        'MS': 'Mississippi',
+        'MT': 'Montana',
+        'NA': 'National',
+        'NC': 'North Carolina',
+        'ND': 'North Dakota',
+        'NE': 'Nebraska',
+        'NH': 'New Hampshire',
+        'NJ': 'New Jersey',
+        'NM': 'New Mexico',
+        'NV': 'Nevada',
+        'NY': 'New York',
+        'OH': 'Ohio',
+        'OK': 'Oklahoma',
+        'OR': 'Oregon',
+        'PA': 'Pennsylvania',
+        'PR': 'Puerto Rico',
+        'RI': 'Rhode Island',
+        'SC': 'South Carolina',
+        'SD': 'South Dakota',
+        'TN': 'Tennessee',
+        'TX': 'Texas',
+        'UT': 'Utah',
+        'VA': 'Virginia',
+        'VI': 'Virgin Islands',
+        'VT': 'Vermont',
+        'WA': 'Washington',
+        'WI': 'Wisconsin',
+        'WV': 'West Virginia',
+        'WY': 'Wyoming'
+}
 
 with open('gerrymander_score_unclean.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
-		for value in row['district_code']:
-			x = value.split('-')
-		reader.update({'newkey':x})
-
-
-print(row['newkey'])
-		#results.append(row)
-
-
+		state, district_number = row['district_code'].split('-') #splits district code into state ("TX") and number ("1")
+		for values in state:
+			for i,j in states.items(): #iterates through state dictionary and returns full state name
+				if state == i:
+					state_full = j
+		clean_data.append({'district_id':state.lower() + district_number, 'state': state_full, 'district_number':district_number, 'gerrymander_score': row['gerrymander_score']})
 
 
 
-"""RANDOM ATTEMPTS
+with open('gerrymander_score_clean.csv', "w") as csvfile:
+        fieldnames = ('district_id', 'state', 'district_number', 'gerrymander_score')
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-filtered_dict = {k:v for (k,v) in data.items() if filter_string in k}
-
-for row in gs_dictionary:
-	print(row['district_code'], row['gerrymander_score'])
-
-print(list(gs_dictionary)
-
-print(row['district_code'], row['gerrymander_score'])
-"""
+        writer.writeheader()
+        for row in clean_data:
+            writer.writerow(row)
